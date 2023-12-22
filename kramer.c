2e0cea8b972d7b368052df4401d47fe086497602
DastRay <104367556+DastRay@users.noreply.github.com>
2023-12-22 21:54:40 +0300

    kramer.c

--- kramer.c
+++ kramer.c
@@ -6,39 +6,39 @@
 
 void readSLAYmatrix(double** A_, double* B_, int n_)
 {
-   printf("Введите коэффиценты СЛАУ (A) и столбец свободных членов (B):\n");
-   for (int i = 0; i < n_; i++)
-   {
-      for (int j = 0; j < n_; j++)
-      {
-         printf("A[%d][%d]: ", i + 1, j + 1);
-         scanf_s("%lf", &A_[i][j]);
-      }
-      printf("B[%d]: ", i + 1);
-      scanf_s("%lf", &B_[i]);
-   }
+    printf("Введите коэффиценты СЛАУ (A) и столбец свободных членов (B):\n");
+    for (int i = 0; i < n_; i++)
+    {
+        for (int j = 0; j < n_; j++)
+        {
+            printf("A[%d][%d]: ", i + 1, j + 1);
+            scanf_s("%lf", &A_[i][j]);
+        }
+        printf("B[%d]: ", i + 1);
+        scanf_s("%lf", &B_[i]);
+    }
 }
 
 void printSLAYmatrix(double** A_, double* B_, int n_)
 {
-   for (int i = 0; i < n_; i++)
-   {
-      for (int j = 0; j < n_; j++)
-      {
-         printf("%8.3lf  ", A_[i][j]);
-      }
-      printf("| %8.3lf\n", B_[i]);
-   }
-   printf("\n");
+    for (int i = 0; i < n_; i++)
+    {
+        for (int j = 0; j < n_; j++)
+        {
+            printf("%8.3lf  ", A_[i][j]);
+        }
+        printf("| %8.3lf\n", B_[i]);
+    }
+    printf("\n");
 }
 
 void replaceColumn(double** matrix_, int rows_, int newCols_, double* newColumn_)
 {
 
-   for (int i = 0; i < rows_; i++) 
-   {
-      matrix_[i][newCols_] = newColumn_[i];
-   }
+    for (int i = 0; i < rows_; i++)
+    {
+        matrix_[i][newCols_] = newColumn_[i];
+    }
 }
 
 double determinantSlay(double** matrix_, double size_)
@@ -96,43 +96,42 @@ double determinantSlay(double** matrix_, double size_)
    return det;
 }
 
-
 void CramerMethod(double** A_, double* B_, int n_)
-{  
-   printf("\n");
-   printf("Расширенная матрица AB:\n");
-   printSLAYmatrix(A_, B_, n_);
-   printf("Решение СЛАУ методом Крамера:\n");
+{
+    printf("\n");
+    printf("Расширенная матрица AB:\n");
+    printSLAYmatrix(A_, B_, n_);
+    printf("Решение СЛАУ методом Крамера:\n");
 
-   double** copy_A = copyMatrix(A_, n_, n_);
-   double det = determinantSlay(copy_A, n_);
+    double** copy_A = copyMatrix(A_, n_, n_);
+    double det = determinantSlay(copy_A, n_);
 
-   if (det == 0)
-   {
-      printf("Определитель равен нулю, СЛАУ имеет бесконечное количество решений или не имеет решений.\nВоспользуйтесь другим способом для решения.\n");
-      return -1;
-   }
-   printf("Определитель матрицы А равен: %.4lf\n", det);
+    if (det == 0)
+    {
+        printf("Определитель равен нулю, СЛАУ имеет бесконечное количество решений или не имеет решений.\nВоспользуйтесь другим способом для решения.\n");
+        return -1;
+    }
+    printf("Определитель матрицы А равен: %.4lf\n", det);
 
-   double* res = malloc(n_ * sizeof(double));
+    double* res = malloc(n_ * sizeof(double));
 
-   
-   for (int k = 0; k < n_; k++)
-   {
-      double** copy = copyMatrix(A_, n_, n_);
-      replaceColumn(copy, n_, k, B_);
-      printf("Дельта %d:\n", k + 1);
-      printMatrix(copy, n_, n_);
 
-      double determ = determinantSlay(copy, n_);
-      printf("Определитель дельта %d = %.3lf\n", k + 1, determ);
-      printf("\n");
-      res[k] = determ / det;
-   }
+    for (int k = 0; k < n_; k++)
+    {
+        double** copy = copyMatrix(A_, n_, n_);
+        replaceColumn(copy, n_, k, B_);
+        printf("Дельта %d:\n", k + 1);
+        printMatrix(copy, n_, n_);
 
-   printf("Решение системы:\n");
-   for (int i = 0; i < n_; i++)
-   {
-      printf("x[%d] = %5.3lf\n", i + 1, res[i]);
-   }
+        double determ = determinantSlay(copy, n_);
+        printf("Определитель дельта %d = %.3lf\n", k + 1, determ);
+        printf("\n");
+        res[k] = determ / det;
+    }
+
+    printf("Решение системы:\n");
+    for (int i = 0; i < n_; i++)
+    {
+        printf("x[%d] = %5.3lf\n", i + 1, res[i]);
+    }
 }
